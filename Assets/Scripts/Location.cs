32faa50eed6beb2fa827094d13f5563a04bfef8d
bioerrorlog <n.iijima.0x01@gmail.com>
2020-10-30 09:27:04 +0900

    get location by Unity LocationService

---
+++ Assets/Scripts/Location.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Location : MonoBehaviour
+{
+    public static Location Instance { set; get; }
+
+    public float latitude;
+    public float longitude;
+    public float altitude;
+
+    private void Start()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        StartCoroutine(StartLocationService());
+    }
+
+    private IEnumerator StartLocationService()
+    {
+        // First, check if user has location service enabled
+        if (!Input.location.isEnabledByUser)
+        {
+            Debug.Log("GPS not enabled");
+            yield break;
+        }
+
+        // Start service before querying location
+        Input.location.Start();
+
+        // Wait until service initializes
+        int maxWait = 20;
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+        // Service didn't initialize in 20 seconds
+        if (maxWait <= 0)
+        {
+            Debug.Log("Timed out");
+            yield break;
+        }
+
+        // Connection has failed
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+            Debug.Log("Unable to determine device location");
+            yield break;
+        }
+
+        // Set locational infomations
+        latitude = Input.location.lastData.latitude;
+        longitude = Input.location.lastData.longitude;
+        altitude = Input.location.lastData.altitude;
+
+        yield break;
+    }
+
+}
