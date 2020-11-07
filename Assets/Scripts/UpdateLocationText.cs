bce3f00e2bcb507e589c4630c2fd3cb288008a98
bioerrorlog <n.iijima.0x01@gmail.com>
2020-11-07 11:46:35 +0900

    location displayed now

--- Assets/Scripts/UpdateLocationText.cs
+++ Assets/Scripts/UpdateLocationText.cs
@@ -6,4 +6,9 @@ using UnityEngine.UI;
 public class UpdateLocationText : MonoBehaviour
 {
     public Text location;
+
+    private void Update()
+    {
+        location.text = $"緯度: " + Location.Instance.latitude + "\n経度: " + Location.Instance.longitude + "\n高度: " + Location.Instance.altitude;
+    }
 }
